6c60f98c690d520c0504406bfd14573c25afbfbb
tomdapchai <vothanhtam2407@gmail.com>
2023-11-11 14:46:58 +0700

    done basic mechanism, might do some interface later

--- works.asm
+++ works.asm
@@ -21,10 +21,14 @@
 	invalidLength:	.asciiz "Invalid input, required length is " #the length
 	overlap:	.asciiz "There is already a ship here, please place elsewhere.\n"
 	
+	gameStart:	.asciiz "Let start the game!\n"
+	turnP1:	.asciiz "Player 1 turn.\n"
+	turnP2:	.asciiz "Player 2 turn. \n"
+	inputTarget:	.asciiz "Enter the coordinate of the target: "
 	invalidTarget:	.asciiz "Invalid target, value must be from 0 to 6.\n"
 	
-	hit:	.asciiz "HIT!.\n"
-	miss:	.asciiz "MISS!.\n"
+	hit:	.asciiz "HIT!\n"
+	miss:	.asciiz "MISS!\n"
 	winP1:	.asciiz "Player 1 WIN!\n"
 	winP2:	.asciiz "Player 2 WIN!\n"
 	newline:	.asciiz "\n"
@@ -44,7 +48,6 @@ main:
 	la $s1, board2
 	
 	
-	
 	# Show instruction
 	li $v0, 4
 	la $a0, instruction
@@ -55,7 +58,8 @@ main:
 	li $v0, 4
 	la $a0, inputP1
 	syscall			
-
+	
+	li $t1, 0		# reset $t1 to loop counter
 dataInputP1:
 	beq $t1, 0, bigP1
 	beq $t1, 1, averageP1
@@ -85,69 +89,144 @@ executeP1:
 exitInputP1:	
 
 	#printOut the board to confirm
-	li $t1, 7 # number of rows
-	li $t2, 7 # number of columns
-	li $t3, 0 # row counter
-	li $t4, 0 # column counter
+	li $v0, 4
+	la $a0, boardP1
+	syscall
+	
+	li $t1, 7 		# number of rows
+	li $t2, 7 		# number of columns
+	li $t3, 0 		# row counter
+	li $t4, 0 		# column counter
 
 print_boardP1:
-    	bge $t3, $t1, exitP1 # if we've printed all rows, exit
-    	li $t4, 0 # reset column counter for new row
+    	bge $t3, $t1, exitP1 	# if we've printed all rows, exit
+    	li $t4, 0 		# reset column counter for new row
 print_rowP1:
-        	bge $t4, $t2, print_newlineP1 # if we've printed all columns in this row, print newline
-        	mul $t5, $t3, $t2 # calculate the linear index
+        	bge $t4, $t2, print_newlineP1 	# if printed all columns in this row, print newline
+        	mul $t5, $t3, $t2 	
         	add $t5, $t5, $t4
-        	sll $t5, $t5, 2 # multiply by 4 because we're dealing with integers
-        	add $t6, $s0, $t5 # calculate the address of the element
-        	lw $a0, 0($t6) # load the integer to be printed
-        	li $v0, 1 # system call code for print_int
+        	mul $t5, $t5, 4 	
+        	add $t5, $s0, $t5 	# $t5 = board1[i]
+        	lw $a0, 0($t5) 		
+        	li $v0, 1 		
         	syscall
-        	addiu $t4, $t4, 1 # increment column counter
+        	addi $t4, $t4, 1 	# increment column counter
         	j print_rowP1
 print_newlineP1:
-        	li $v0, 4 # system call code for print_string
-        	la $a0, newline # address of newline character
+        	li $v0, 4 		
+        	la $a0, newline 	
         	syscall
-        	addiu $t3, $t3, 1 # increment row counter
+        	addi $t3, $t3, 1 	# row counter ++
         	j print_boardP1
 exitP1:
 
 	# P2
 	li $v0, 4
 	la $a0, inputP2
-	syscall	
+	syscall
+	li $t1, 0		# reset $t1 to loop counter	
 
-#	add $a0, $s1, 0
-#dataInputP2:
-#	beq $t1, 3, exitInputP2
-#	add $a1, $t1, $zero
-#	jal input
-#	addi $t1, $t1, 1
-#	j dataInputP2
-#exitInputP2:
+dataInputP2:
+	beq $t1, 0, bigP2
+	beq $t1, 1, averageP2
+	beq $t1, 2, smallP2
+bigP2:
+	li $v0, 4
+	la $a0, bigInput
+	syscall
+	j executeP2
+averageP2:
+	li $v0, 4
+	la $a0, averageInput
+	syscall
+	j executeP2
+smallP2:
+	li $v0, 4
+	la $a0, smallInput
+	syscall
+executeP2:
+	la $a0, board2
+	addi $a1, $t1, 1	# input from big > average > small
+	jal input
+	
+	addi $t1, $t1, 1
+	beq $t1, 3, exitInputP2
+	j dataInputP2
+exitInputP2:	
+
+	#printOut the board to confirm
+	li $v0, 4
+	la $a0, boardP2
+	syscall
+	
+	li $t1, 7 # number of rows
+	li $t2, 7 # number of columns
+	li $t3, 0 # row counter
+	li $t4, 0 # column counter
+
+print_boardP2:
+    	bge $t3, $t1, exitP2 # if we've printed all rows, exit
+    	li $t4, 0 # reset column counter for new row
+print_rowP2:
+        	bge $t4, $t2, print_newlineP2 # if we've printed all columns in this row, print newline
+        	mul $t5, $t3, $t2 # calculate the linear index
+        	add $t5, $t5, $t4
+        	sll $t5, $t5, 2 # multiply by 4 because we're dealing with integers
+        	add $t5, $s1, $t5 # calculate the address of the element
+        	lw $a0, 0($t5) # load the integer to be printed
+        	li $v0, 1 # system call code for print_int
+        	syscall
+        	addi $t4, $t4, 1 # increment column counter
+        	j print_rowP2
+print_newlineP2:
+        	li $v0, 4 # system call code for print_string
+        	la $a0, newline # address of newline character
+        	syscall
+        	addi $t3, $t3, 1 # increment row counter
+        	j print_boardP2
+exitP2:
+
 
 # Game start				
 	# set hit counter for two players
-#	li $s2, 0	# hit_counter_P1
-#	li $s3, 0	# hit_counter_P2
+	li $s2, 0	# hit_counter_P1
+	li $s3, 0	# hit_counter_P2
 
-#while:
-#	# things goes here
-#	beq $s2, 16, P1		# Player 1 WIN, goto P1
-#	beq $s3, 16, P2		# Player 2 WIN, goto P2
-	# things goes here
+    	li $v0, 4
+    	la $a0, gameStart
+    	syscall
+
+while:
+	# things goes here, mechanism and everything
+    	li $v0, 4
+    	la $a0, turnP1
+    	syscall
+    	la $a1, board2          # store board2 to $a1
+    	li $a2, 1               # mode 1, player 1
+    	jal game                # turn works with two arguments $a1, $a2
+
+    	li $v0, 4
+    	la $a0, turnP2
+    	syscall
+    	la $a1, board1
+    	li $a2, 2
+    	jal game
+
+	beq $s2, 16, P1win		# Player 1 WIN, goto P1
+	beq $s3, 16, P2win		# Player 2 WIN, goto P2
+    	j while
 	
 	
-#P1:
-#	li $v0, 4
-#	la $a0, winP1
-#	syscall
-#	j end
-#P2:
-#	li $v0, 4
-#	la $a0, winP2
-#	syscall
-#end:
+P1win:
+	li $v0, 4
+	la $a0, winP1
+	syscall
+	j end
+P2win:
+	li $v0, 4
+	la $a0, winP2
+	syscall
+end:
 	# stop the program
 	li $v0, 10
 	syscall
@@ -170,7 +249,7 @@ inputLoop:			# n times, n = 1, 2, 3
 	syscall
 	
 	li $v0, 1
-	addi $a0, $t1, 0
+	addi $a0, $t1, 1
 	syscall
 	
 	li $v0, 4
@@ -230,7 +309,7 @@ checkSize:
 	beq $t2, $zero, byRow
 	beq $t3, $zero, byColumn
 	j errorPos
-	#Perform checking overlap while assign 1 (very hard)
+	# perform assign 0 to 1, if overlap undo and return error
 byRow:
 	mul $t0, $s2, 7		# $t0 = row_idx * 7
 	add $t0, $t0, $s3	# $t0 = row_idx * 7 + column_idx
@@ -241,7 +320,6 @@ loopRow:
 	lw $t6, 0($t0)
 	bne $t6, $zero, undoRow
 	# No overlaping detected, assgin 0 to 1
-
 	li $t6, 1
 	sw $t6, 0($t0)
 	
@@ -258,12 +336,12 @@ continueRow:
 	#starting trace back and undo assigning 0 to 1	
 undoRow:	
 	beq $t4, $zero, endUndoRow
-	beq $t5, $zero, undoBackwardRow
+	beq $t5, $zero, undoBackward
 	sub $t0, $t0, 4
-	j continueUndo
-undoBackwardRow:
+	j continueUndoRow
+undoBackward:
 	addi $t0, $t0, 4
-continueUndo:
+continueUndoRow:
 	li $t6, 0
 	sw $t6, 0($t0)
 	sub $t4, $t4, 1
@@ -278,6 +356,10 @@ byColumn:
 	mul $t0, $t0, 4		
 	add $t0, $t0, $s5	# $t0 now is board_i[row_idx][column_idx]
 loopColumn:	
+	# Check overlaping
+	lw $t6, 0($t0)
+	bne $t6, $zero, undoColumn
+	# No overlaping detected, assign 0 to 1
 	li $t6, 1
 	sw $t6, 0($t0)
 	
@@ -290,6 +372,20 @@ continueColumn:
 	addi $t4, $t4, 1
 	beq $t4, $s4, finish
 	j loopColumn
+undoColumn:
+	beq $t4, $zero, endUndoColumn
+	beq $t5, $zero, undoUpsidedown
+	sub $t0, $t0, 28
+	j continueUndoColumn
+undoUpsidedown:
+	addi $t0, $t0, 28
+continueUndoColumn:
+	li $t6, 0
+	sw $t6, 0($t0)
+	sub $t4, $t4, 1
+	j undoColumn
+endUndoColumn:
+	j errorOverlap
 
 finish:
 	addi $t1, $t1, 1		# $t1++
@@ -330,10 +426,73 @@ exitInput:
 	addi $sp, $sp, 8
 	jr $ra
 
+game:
+        	# $a1 is board, $a2 is player
+        	addi $sp, $sp, -4
+        	sw $ra, 0($sp)
+        	addi $t0, $a2, 0        # $t0 is player
+
+targetLoop:
+        	li $v0, 4
+        	la $a0, inputTarget
+        	syscall
+
+        	li $v0, 5               # input x
+        	syscall
+        	addi $a2, $v0, 0
+        	jal isValid
+        	beq $v0, $zero, errorTarget
+        	addi $t1, $a2, 0        # store x
+
+        	li $v0, 5               # input y
+        	syscall
+        	addi $a2, $v0, 0
+        	jal isValid
+        	beq $v0, $zero, errorTarget
+        	addi $t2, $a2, 0        # store y
+
+        	mul $t3, $t1, 7         # t3 is index, $t3 = x * 7
+        	add $t3, $t3, $t2       # t3 = x * 7 + y
+        	mul $t3, $t3, 4
+        	add $t3, $t3, $a1       # $t3 now at board_i[x][y]
+
+        	lw $t4, 0($t3)          # load value at board_i[x][y] to $t4
+        	beq $t4, $zero, targetMiss
+        	li $v0, 4
+        	la $a0, hit
+        	syscall
+
+        	li $t4, 0
+        	sw $t4, 0($t3)          # reset 1 to 0
+
+        	bne $t0, 1, hitP2
+        	addi $s2, $s2, 1
+        	j exitGame
+hitP2:
+        	addi $s3, $s3, 1
+        	j exitGame
+
+targetMiss:
+        	li $v0, 4
+        	la $a0, miss
+        	syscall
+        	j exitGame
+
+errorTarget:
+        	li $v0, 4
+        	la $a0, invalidTarget
+        	syscall
+        	j targetLoop
+
+exitGame:
+        	lw $ra, 0($sp)
+        	addi $sp, $sp, 4
+        	jr $ra
 
 isValid:
-	slti $v0, $a2, 7
-	addi $t6, $zero, -1
-	slt $t6, $t6, $a2
-	and $v0, $v0, $t6
-	jr $ra
+        	slti $v0, $a2, 7
+        	addi $t6, $zero, -1
+        	slt $t6, $t6, $a2
+        	and $v0, $v0, $t6
+        	jr $ra
+
