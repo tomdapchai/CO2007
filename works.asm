72af7d7fd0ad2b40e5fcb4ec25c9810769342fd4
tomdapchai <vothanhtam2407@gmail.com>
2023-11-18 15:55:39 +0700

    should be good now

--- works.asm
+++ works.asm
@@ -1,42 +1,3 @@
-.data
-	# Initialize two boards with all elements are 0
-	board1:	.word 0:49
-	board2:	.word 0:49
-	ip:	.space 1000
-	
-	welcome:	.asciiz "Welcome to BATTLESHIP game!\n"
-	instruction:	.asciiz "To start, each player have to give the coordinate of 3 2x1 ships, 2 3x1 ships, 1 4x1 ships with format <row_bow> <column_bow> <row_stern> <column_stern> \nFor example: a 4x1 ship has the coordinate of bow is (0, 0) and stern is (0, 3) will be entered as 0 0 0 3.\n"
-	inputP1:	.asciiz "Player 1 input: \n"
-	inputP2:	.asciiz "Player 2 input: \n"
-	boardP1:	.asciiz "Map of Player 1: \n"
-	boardP2:	.asciiz "Map of Player 2: \n"
-	
-	smallInput:	.asciiz "2x1 ships input: \n"
-	averageInput:	.asciiz "3x1 ships input: \n"
-	bigInput:	.asciiz "4x1 ships input: \n"
-	shipInput:	.asciiz "Enter the coordinate of the ship: \n"
-	shipShow:	.asciiz "Ship "
-
-	invalidRange:	.asciiz "Invalid input, value must be from 0 to 6.\n"
-	invalidPos:	.asciiz "Invalid input, bow and stern must be on the same row or same column.\n"
-	invalidLength:	.asciiz "Invalid input, required length is " #the length
-	overlap:	.asciiz "There is already a ship here, please place elsewhere.\n"
-	
-	gameStart:	.asciiz "Let start the game!\n"
-	turnP1:	.asciiz "Player 1 turn.\n"
-	turnP2:	.asciiz "Player 2 turn. \n"
-	inputTarget:	.asciiz "Enter the coordinate of the target: "
-	invalidTarget:	.asciiz "Invalid target, value must be from 0 to 6.\n"
-	
-	hit:	.asciiz "HIT!\n"
-	miss:	.asciiz "MISS!\n"
-	winP1:	.asciiz "Player 1 WIN!\n"
-	winP2:	.asciiz "Player 2 WIN!\n"
-	newline:	.asciiz "\n"
-	space:		.asciiz " "
-	check:	.asciiz "check\n"
-	check1:	.asciiz "check1\n"
-	arrCheck:	.asciiz "arrCheck\n"
 .text
 main:
 	# Welcome prompt
@@ -44,10 +5,12 @@ main:
 	la $a0, welcome
 	syscall
 	
-	# Load two boards to $s0 and $s1 register
-	la $s0, board1
-	la $s1, board2
+	# Reset all board to 0 in case players want to play again
+	la $a0, board1
+	jal resetBoard
 	
+	la $a0, board2
+	jal resetBoard
 	
 	# Show instruction
 	li $v0, 4
@@ -94,38 +57,35 @@ exitInputP1:
 	la $a0, boardP1
 	syscall
 	
-	li $t1, 7 		# number of rows
-	li $t2, 7 		# number of columns
-	li $t3, 0 		# row counter
-	li $t4, 0 		# column counter
-
-print_boardP1:
-    	bge $t3, $t1, exitP1 	# if we've printed all rows, exit
-    	li $t4, 0 		# reset column counter for new row
-print_rowP1:
-        	bge $t4, $t2, print_newlineP1 	# if printed all columns in this row, print newline
-        	mul $t5, $t3, $t2 	
-        	add $t5, $t5, $t4
-        	mul $t5, $t5, 4 	
-        	add $t5, $s0, $t5 	# $t5 = board1[i]
-        	lb $a0, 0($t5) 		
-        	li $v0, 1 		
-        	syscall
-        	
-        	la $a0, space
-        	li $v0, 4
-        	syscall
-        	
-        	addi $t4, $t4, 1 	# increment column counter
-        	j print_rowP1
-print_newlineP1:
-        	li $v0, 4 		
-        	la $a0, newline 	
-        	syscall
-        	addi $t3, $t3, 1 	# row counter ++
-        	j print_boardP1
-exitP1:
-
+	la $a2, board1
+	jal printBoard
+	
+	# Option if player want to place the ships again, or end the game, or continue
+	li $v0, 4
+	la $a0, reThink
+	syscall
+selectP1:
+	li $v0, 4
+	la $a0, rePlace
+	syscall
+	
+	li $v0, 8
+	la $a0, ip
+	li $a1, 1000
+	syscall
+	
+	lb $t1, 0($a0)
+	beq $t1, 49, continuePlayP1
+	beq $t1, 50, rePlaceP1
+	beq $t1, 51, end
+	j selectP1
+rePlaceP1:
+	la $a0, board1
+	jal resetBoard
+	li $t1, 0
+	j dataInputP1
+	
+continuePlayP1:
 	# P2
 	li $v0, 4
 	la $a0, inputP2
@@ -164,40 +124,34 @@ exitInputP2:
 	li $v0, 4
 	la $a0, boardP2
 	syscall
+
+	la $a2, board2
+	jal printBoard
+	# Option if player want to place the ships again, or end the game, or continue
+	li $v0, 4
+	la $a0, reThink
+	syscall
+selectP2:
+	li $v0, 4
+	la $a0, rePlace
+	syscall
 	
-	li $t1, 7 # number of rows
-	li $t2, 7 # number of columns
-	li $t3, 0 # row counter
-	li $t4, 0 # column counter
-
-print_boardP2:
-    	bge $t3, $t1, exitP2 # if we've printed all rows, exit
-    	li $t4, 0 # reset column counter for new row
-print_rowP2:
-        	bge $t4, $t2, print_newlineP2 # if we've printed all columns in this row, print newline
-        	mul $t5, $t3, $t2 # calculate the linear index
-        	add $t5, $t5, $t4
-        	sll $t5, $t5, 2 # multiply by 4 because we're dealing with integers
-        	add $t5, $s1, $t5 # calculate the address of the element
-        	lw $a0, 0($t5) # load the integer to be printed
-        	li $v0, 1 # system call code for print_int
-        	syscall
-        	
-        	la $a0, space
-        	li $v0, 4
-        	syscall
-        	
-        	addi $t4, $t4, 1 # increment column counter
-        	j print_rowP2
-print_newlineP2:
-        	li $v0, 4 # system call code for print_string
-        	la $a0, newline # address of newline character
-        	syscall
-        	addi $t3, $t3, 1 # increment row counter
-        	j print_boardP2
-exitP2:
-
-
+	li $v0, 8
+	la $a0, ip
+	li $a1, 1000
+	syscall
+	
+	lb $t1, 0($a0)
+	beq $t1, 49, continuePlayP2
+	beq $t1, 50, rePlaceP2
+	beq $t1, 51, end
+	j selectP1
+rePlaceP2:
+	la $a0, board2
+	jal resetBoard
+	li $t1, 0
+	j dataInputP2
+continuePlayP2:
 # Game start				
 	# set hit counter for two players
 	li $s2, 0	# hit_counter_P1
@@ -215,38 +169,69 @@ while:
     	la $a1, board2          # store board2 to $a1
     	li $a2, 1               # mode 1, player 1
     	jal game                # turn works with two arguments $a1, $a2
-
+	beq $s2, 16, P1win		# Player 1 WIN, goto P1
+	
     	li $v0, 4
     	la $a0, turnP2
     	syscall
     	la $a1, board1
     	li $a2, 2
     	jal game
-
-	beq $s2, 16, P1win		# Player 1 WIN, goto P1
 	beq $s3, 16, P2win		# Player 2 WIN, goto P2
     	j while
-	
 	
 P1win:
 	li $v0, 4
 	la $a0, winP1
 	syscall
-	j end
+	j endGame
 P2win:
 	li $v0, 4
 	la $a0, winP2
 	syscall
+endGame:
+	li $v0, 4
+	la $a0, replay
+	syscall
+	
+	li $v0, 4
+	la $a0, yes
+	syscall
+	
+	li $v0, 4
+	la $a0, no
+	syscall
+	
+selectLoop:	
+	li $v0, 4
+	la $a0, selection
+	syscall
+
+	li $v0, 8
+	la $a0, ip
+	li $a1, 1000
+	syscall
+	
+	lb $t0, 0($a0)
+	beq $t0, 49, main	# 1
+	beq $t0, 50, end	# 2
+	# invalid choice
+	j selectLoop
 end:
 	# stop the program
+	li $v0, 4
+	la $a0, thanks
+	syscall
+	
 	li $v0, 10
 	syscall
 
 # Function area
 input:	
 	# set index & counter
-	addi $sp, $sp, -4
+	addi $sp, $sp, -8
 	sw $t1, 0($sp)
+	sw $ra, 4($sp)
 	li $t0, 0		# index
 	li $t1, 0		# counter
 	addi $s5, $a0, 0	# store the board to $s5
@@ -277,7 +262,7 @@ inputLoop:			# n times, n = 1, 2, 3
 	addi $t6, $a0, 0	# t6 contains input string
 	li $s2, 0
 	lb $t8, 7($t6)
-	bne $t8, '\n', errorRange
+	bne $t8, '\n', errorFormat
 checkInputLoop:
 	lb $s3, 0($t6)
 	div $t2, $s2, 2
@@ -324,6 +309,12 @@ endCheckInput:
 negative:
 	sub $t4, $zero, $t4
 checkSize:
+	#errorPosCheck
+	bne $t2, $zero, checkPos
+	j doneCheckPos
+checkPos:
+	bne $t3, $zero, errorPos
+doneCheckPos:
 	bne $t4, $s4, errorLength
 	# Initialize ship in board, change from 0 to 1
 	li $t4, 0
@@ -331,7 +322,6 @@ checkSize:
 	# addi $s4, $s4, 1	# restore true required size
 	beq $t2, $zero, byRow
 	beq $t3, $zero, byColumn
-	j errorPos
 	# perform assign 0 to 1, if overlap undo and return error
 byRow:
 	mul $t0, $s2, 7		# $t0 = row_idx * 7
@@ -411,8 +401,19 @@ endUndoColumn:
 	j errorOverlap
 
 finish:
+	li $v0, 4
+	la $a0, currentBoard
+	syscall
+	
+	addi $a2, $s5, 0
+	jal printBoard
 	addi $t1, $t1, 1		# $t1++
 	beq $t1, $a1, exitInput
+	j keepInput
+errorFormat:
+	li $v0, 4
+	la $a0, invalidFormat
+	syscall
 	j keepInput
 errorRange:
 	li $v0, 4
@@ -430,7 +431,7 @@ errorLength:
 	syscall
 	
 	li $v0, 1
-	addi $a0, $s4, 0
+	addi $a0, $s4, 1
 	syscall
 	
 	li $v0, 4
@@ -445,7 +446,8 @@ keepInput:
 	j inputLoop
 exitInput:	
 	lw $t1, 0($sp)
-	addi $sp, $sp, 4
+	lw $ra, 4($sp)
+	addi $sp, $sp, 8
 	jr $ra
 
 game:
@@ -455,46 +457,48 @@ game:
         	addi $t0, $a2, 0        # $t0 is player
 
 targetLoop:
-		# input coordinate
+	# input coordinate
         	li $v0, 4
         	la $a0, inputTarget
         	syscall
 
-		addi $t1, $a1, 0 	# store $a1
+	addi $t1, $a1, 0 	# store $a1
 		
-		li $v0, 8
-		la $a0, ip
-		li $a1, 1000
-		syscall
+	li $v0, 8
+	la $a0, ip
+	li $a1, 1000
+	syscall
 		
-		addi $t2, $a0, 0	# $t2 contains input string
-		addi $a1, $t1, 0	# restore $a1, $t1 have th
-		
-		li $t1, 0		# counter
+	addi $t2, $a0, 0	# $t2 contains input string
+	addi $a1, $t1, 0	# restore $a1, $t1 have th
+	
+	lb $t8, 3($t2)
+	bne $t8, '\n', errorFormatTarget
+	li $t1, 0		# counter
 checkGameInput:
-		lb $t3, 0($t2) 
-		div $t4, $t1, 2
-		mfhi $t4
-		bne $t4, $zero, ifOddGame
-		li $t5, 55
-		bge $t3, $t5, errorTarget
-		bgt $zero, $t3, errorTarget
-		j keepCheckGame	
+	lb $t3, 0($t2) 
+	div $t4, $t1, 2
+	mfhi $t4
+	bne $t4, $zero, ifOddGame
+	li $t5, 55
+	bge $t3, $t5, errorTarget
+	bgt $zero, $t3, errorTarget
+	j keepCheckGame	
 ifOddGame:
-		li $t5, 32
-		bne $t3, $t5, errorTarget
+	li $t5, 32
+	bne $t3, $t5, errorTarget
 keepCheckGame:
-		addi $t1, $t1, 1
-		addi $t2, $t2, 1
-		bge $t1, 3, endCheckGame
-		j checkGameInput
+	addi $t1, $t1, 1
+	addi $t2, $t2, 1
+	bge $t1, 3, endCheckGame
+	j checkGameInput
 endCheckGame:
-		sub $t2, $t2, 3
-		lb $t1, 0($t2)		# x + 48
-		lb $t2, 2($t2)		# y + 48
+	sub $t2, $t2, 3
+	lb $t1, 0($t2)		# x + 48
+	lb $t2, 2($t2)		# y + 48
 		
-		sub $t1, $t1, 48	# x
-		sub $t2, $t2, 48	# y
+	sub $t1, $t1, 48	# x
+	sub $t2, $t2, 48	# y
 
         	mul $t3, $t1, 7         # t3 is index, $t3 = x * 7
         	add $t3, $t3, $t2       # t3 = x * 7 + y
@@ -506,9 +510,8 @@ endCheckGame:
         	li $v0, 4
         	la $a0, hit
         	syscall
-
-        	li $t4, 0
-        	sw $t4, 0($t3)          # reset 1 to 0
+        	
+        	sw $zero, 0($t3)          # reset 1 to 0
 
         	bne $t0, 1, hitP2
         	addi $s2, $s2, 1
@@ -522,7 +525,11 @@ targetMiss:
         	la $a0, miss
         	syscall
         	j exitGame
-
+errorFormatTarget:
+	li $v0, 4
+	la $a0, formatTarget
+	syscall
+	j targetLoop
 errorTarget:
         	li $v0, 4
         	la $a0, invalidTarget
@@ -533,10 +540,105 @@ exitGame:
         	lw $ra, 0($sp)
         	addi $sp, $sp, 4
         	jr $ra
+        	
+printBoard:
+	addi $sp, $sp, -4
+	sw $t1, 0($sp)
+	li $t1, 7 		# number of rows
+	li $t2, 7 		# number of columns
+	li $t3, 0 		# row counter
+	li $t4, 0 		# column counter
+startPrint:
+	# function to shows the board after each ship placement
+    	bge $t3, $t1, exitBoard 	# if we've printed all rows, exit
+    	li $t4, 0 		# reset column counter for new row
+printRow:
+        	bge $t4, $t2, printNewline 	# if printed all columns in this row, print newline
+        	mul $t5, $t3, $t2 	
+        	add $t5, $t5, $t4
+        	mul $t5, $t5, 4 	
+        	add $t5, $a2, $t5 	# $t5 = board1[i]
+        	lb $a0, 0($t5) 		
+        	li $v0, 1 		
+        	syscall
+        	
+        	la $a0, space
+        	li $v0, 4
+        	syscall
+        	
+        	addi $t4, $t4, 1 	# increment column counter
+        	j printRow
+printNewline:
+        	li $v0, 4 		
+        	la $a0, newline 	
+        	syscall
+        	addi $t3, $t3, 1 	# row counter ++
+        	j startPrint
+exitBoard:
+	lw $t1, 0($sp)
+	addi $sp, $sp, 4
+	jr $ra
+	
+resetBoard:
+    	li $t0, 0
+loopReset:
+   	mul $t2, $t0, 4       
+    	add $t3, $t2, $a0     
 
-isValid:
-        	slti $v0, $a2, 7
-        	addi $t6, $zero, -1
-        	slt $t6, $t6, $a2
-        	and $v0, $v0, $t6
-        	jr $ra
+    	sw $zero, 0($t3)       
+
+    	addi $t0, $t0, 1 
+    	bne $t0, 49, loopReset
+	
+    	jr $ra
+
+# Data field
+.data
+	# Initialize two boards with all elements are 0
+	board1:	.word 0:49
+	board2:	.word 0:49
+	# Create input field
+	ip:	.space 1000
+	
+	welcome:	.asciiz "Welcome to BATTLESHIP game!\n"
+	replay:	.asciiz "Want to play again?\n"
+	reThink:	.asciiz "Do you want to place the ships again, or end the game, or continue?\n"
+	rePlace:	.asciiz "Plese type 1 to continue, 2 to place the ship again and 3 for exit the game.\n"
+	yes:	.asciiz "1. Play again.\n"
+	no:	.asciiz "2. Exit.\n"
+	
+	selection:	.asciiz "Please type 1 for Play again, 2 for Exit.\n"
+	instruction:	.asciiz "To start, each player have to give the coordinate of 3 2x1 ships, 2 3x1 ships, 1 4x1 ships with format <row_bow> <column_bow> <row_stern> <column_stern> \nFor example: a 4x1 ship has the coordinate of bow is (0, 0) and stern is (0, 3) will be entered as 0 0 0 3.\n"
+	inputP1:	.asciiz "Player 1 input: \n"
+	inputP2:	.asciiz "Player 2 input: \n"
+	currentBoard:	.asciiz "Current map: \n"
+	boardP1:	.asciiz "Map of Player 1: \n"
+	boardP2:	.asciiz "Map of Player 2: \n"
+	
+	smallInput:	.asciiz "2x1 ships input: \n"
+	averageInput:	.asciiz "3x1 ships input: \n"
+	bigInput:	.asciiz "4x1 ships input: \n"
+	shipInput:	.asciiz "Enter the coordinate of the ship: \n"
+	shipShow:	.asciiz "Ship "
+
+	invalidFormat:	.asciiz "Invalid input, you must type in 4 numbers from 0 to 6 for the coordinate of bow and stern!\n"
+	invalidRange:	.asciiz "Invalid input, value must be from 0 to 6!\n"
+	invalidPos:	.asciiz "Invalid input, bow and stern must be on the same row or same column!\n"
+	invalidLength:	.asciiz "Invalid input, required length is " #the length
+	overlap:	.asciiz "There is already a ship here, please place elsewhere!\n"
+	
+	gameStart:	.asciiz "Let start the game!\n"
+	turnP1:	.asciiz "Player 1 turn.\n"
+	turnP2:	.asciiz "Player 2 turn. \n"
+	inputTarget:	.asciiz "Enter the coordinate of the target: "
+	formatTarget:	.asciiz "Invalid target, you must type in 2 numbers from 0 to 6 for the coordinate of the target!\n"
+	invalidTarget:	.asciiz "Invalid target, value of two coordinates must be from 0 to 6!\n"
+
+	hit:	.asciiz "HIT!\n"
+	miss:	.asciiz "MISS!\n"
+	winP1:	.asciiz "Player 1 WIN!\n"
+	winP2:	.asciiz "Player 2 WIN!\n"
+	
+	thanks:	.asciiz "Thanks for playing, hope you enjoy the game!"
+	newline:	.asciiz "\n"
+	space:	.asciiz " "
